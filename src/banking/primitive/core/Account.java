de2c666599937cbc5b162111066f3716b3077f06
Clayton Auzenne <cjauzenn@asu.edu>
2017-04-10 03:35:27 -0600

    bug-1, updating javadoc comments for codinmg standards

--- src/banking/primitive/core/Account.java
+++ src/banking/primitive/core/Account.java
@@ -1,5 +1,17 @@
+/*
+  File:	Account.java
+  Author: Clayton Auzenne
+  Date:	4/10/2017
+  
+  Description: Base for all account types
+*/
 package banking.primitive.core;
 
+/**
+Class: Account
+
+Description: Abstract interface for all account types
+*/
 public abstract class Account implements java.io.Serializable {
     private static final long serialVersionUID = 1L;
 
@@ -35,25 +47,27 @@ public abstract class Account implements java.io.Serializable {
         return balance;
     }
 
-    /**
-     * Adds money to an account. May not be done if the account is CLOSED
-     * 
-     * @param parameter
-     *            amount is a deposit and must be > 0
-     * @return true if the deposit was successful, false if not due to amount or
-     *         invalid state
-     */
+/**
+    Method: deposit
+    Inputs: @param amount
+               	amount is a deposit and must be > 0
+    Returns: @return true if the deposit was successful, false if not due to amount or
+             	invalid state
+
+    Description: Adds money to an account. May not be done if the account is CLOSED
+*/
     public abstract boolean deposit(float amount);
 
-    /**
-     * Takes money out of an account. If the balance falls below 0 then the
-     * account is moved to an OVERDRAWN state
-     * 
-     * @param parameter
-     *            amount is a withdrawal and must be > 0
-     * @return true if the deposit was successful, false if not due to amount or
-     *         invalid state
-     */
+/**
+    Method: withdraw
+    Inputs: @param amount
+            	amount is a withdrawal and must be > 0
+    Returns: @return true if the deposit was successful, false if not due to amount or
+             	invalid state
+
+    Description: Takes money out of an account. If the balance falls below 0 then the
+    	account is moved to an OVERDRAWN state
+*/
     public abstract boolean withdraw(float amount);
 
     /**
